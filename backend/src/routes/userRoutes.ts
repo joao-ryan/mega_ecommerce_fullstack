08b130a53629338b66b3f305e615daddef068b57
João Ryan <joaoryanmaranhaodecastro@gmail.com>
2026-08-15 10:14:07 -0300

    revert: remove integracao com clerk e restaura autenticacao JWT nativa

--- backend/src/routes/userRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { syncClerkUser } from "../controllers/userController.js";
-
-const router = Router();
-
-router.post("/sync", syncClerkUser);
-
-export default router;
